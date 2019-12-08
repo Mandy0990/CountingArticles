90ece4a857e9bd8618235e64bb32f2cbb1a30f56
Armando <apiedra0990@gmail.com>
2019-12-07 21:30:35 -0500

    Add DataBinding to Proyect

--- app/src/main/java/com/example/countingarticles/MainActivity.kt
+++ app/src/main/java/com/example/countingarticles/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
@@ -14,17 +15,19 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.countingarticles.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration : AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         setSupportActionBar(findViewById(R.id.toolbar))
         setTitle("Counting Article")
-
     }
 
 
--- app/src/main/java/com/example/countingarticles/articles/ArticleFragment.kt
+++ app/src/main/java/com/example/countingarticles/articles/ArticleFragment.kt
@@ -4,9 +4,11 @@ package com.example.countingarticles.articles
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.countingarticles.R
+import com.example.countingarticles.databinding.FragmentArticleBinding
 
 
 /**
@@ -15,7 +17,7 @@ import com.example.countingarticles.R
 class ArticleFragment : Fragment() {
 
     private lateinit var viewModel: ArticleViewModel
-    //private lateinit var binding: ArticleFragmentBinding
+    private lateinit var binding: FragmentArticleBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,10 +26,18 @@ class ArticleFragment : Fragment() {
         // Inflate the layout for this fragment
         setHasOptionsMenu(true)
 
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_article,
+            container,
+            false
+        )
+
         Log.i("GameFragment", "Called ViewModelProviders.of")
         viewModel = ViewModelProviders.of(this).get(ArticleViewModel::class.java)
 
-        return inflater.inflate(R.layout.fragment_article, container, false)
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
